7a1bc44fc4a0b1c59fa7f2a114582eff6bd6d54c
Jacob Plaster <jacob.s.plaster@gmail.com>
2023-03-04 13:22:41 +0000

    feat: add dcn mainnet deployment

---
+++ blockbooks/dcn/mainnet/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /opt/coins/blockbook/iochain/
+
+./bin/blockbook -sync \
+    -blockchaincfg=/app/blockchaincfg.json \
+    -internal=:9030 \
+    -public=:9130 \
+    -resyncindexperiod=5000 \
+    -resyncmempoolperiod=5000 \
+    -blockheight=0 \
+    -workers=2 \
+    -debug=true \
+    -logtostderr
