a3cfd3de30127fbe23550abc69d7085a86d36f1b
syrohei <syrohei@gmail.com>
2023-04-14 02:02:14 +0900

    feat: support geth v1.11.5 and blockbook v0.4.0

--- blockbooks/ethereum/mainnet/entrypoint.sh
+++ blockbooks/ethereum/mainnet/entrypoint.sh
@@ -8,6 +8,6 @@ cd /opt/coins/blockbook/ethereum/
     -public=:9130 \
     -resyncindexperiod=5000 \
     -resyncmempoolperiod=5000 \
-    -blockheight=15657586 \
+    -blockheight=16819084 \
     -workers=1 \
     -logtostderr
